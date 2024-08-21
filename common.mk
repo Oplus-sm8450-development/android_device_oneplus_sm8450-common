7947d79121dc1b58cb8568cc6841717d0c8599d8
Bruno Martins <bgcngm@gmail.com>
2024-08-22 00:33:31 +0100

    sm8450-common: Drop 32-bit audio HAL and its dependencies
    * Following sm8550 changes
    * We alredy droped 32bit blobs
    
    Change-Id: I8edf07b896f286c9e55eb1cbf963d02bd8be6ab0

--- common.mk
+++ common.mk
@@ -52,6 +52,7 @@ PRODUCT_PACKAGES += \
     libagm_compress_plugin \
     libagm_mixer_plugin \
     libagm_pcm_plugin \
+    libaudioroute.vendor \
     libbatterylistener \
     libeffectproxy \
     libfmpal \
