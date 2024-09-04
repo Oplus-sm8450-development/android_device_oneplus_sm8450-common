786ad6b6f41aea469f7e9148255923173166a327
Machad3x <machad3x@gmail.com>
2024-09-04 13:55:26 -0400

    sm8450-common: Silence smpammy and error logs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,6 +72,7 @@ TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/configs/props/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/configs/props/product.prop
+TARGET_SYSTEM_PROP += $(COMMON_PATH)/configs/props/system.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/configs/props/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/configs/props/vendor.prop
 
