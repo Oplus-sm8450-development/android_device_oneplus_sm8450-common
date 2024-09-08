ca2d507d600cfef203093ee81dfcaf58c8b19d78
Machad3x <machad3x@gmail.com>
2024-09-08 19:16:38 -0400

    sm8450-common: Drop LiveDisplay
    We're using AOSP Colors

--- common.mk
+++ common.mk
@@ -341,10 +341,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.oplus
-
 # Logging
 SPAMMY_LOG_TAGS := \
     MiStcImpl \
