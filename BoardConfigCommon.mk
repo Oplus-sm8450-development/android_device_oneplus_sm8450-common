1ea415aa7682ebae1f13c4919e61ed412662b212
Machad3x <machad3x@gmail.com>
2024-09-30 20:43:03 -0400

    sm8450-common: Use clang-r487747c for kernel compilation

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -122,6 +122,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 BOARD_USES_GENERIC_KERNEL_IMAGE := true
 BOARD_USES_QCOM_MERGE_DTBS_SCRIPT := true
+TARGET_KERNEL_CLANG_VERSION := r487747c
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8450
 TARGET_KERNEL_CONFIG := gki_defconfig vendor/waipio_GKI.config vendor/oplus_GKI.config vendor/debugfs.config
 
