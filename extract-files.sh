a0f6f30030e30289adde7b59b6ffa4d25b285ab7
Machad3x <machad3x@gmail.com>
2024-11-07 15:10:47 -0400

    sm8450-common: Remove libcodec2_shim

--- extract-files.sh
+++ extract-files.sh
@@ -143,10 +143,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libhidlbase_shim.so" "${2}"
             ;;
-        vendor/lib64/libqcodec2_core.so)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF}" --add-needed "libcodec2_shim.so" "${2}"
-            ;;
         vendor/etc/seccomp_policy/atfwd@2.0.policy|vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
             [ "$2" = "" ] && return 0
             grep -q "gettid: 1" "${2}" || echo -e "\ngettid: 1" >> "${2}"
